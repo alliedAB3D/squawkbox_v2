001ff62aa34672856bde2dbdedb2738bc2f85745
clintonholladay <j1513holladay@gmail.com>
2021-07-06 17:02:26 -0500

    print back ASCII letter

--- Code Testing/main.cpp
+++ Code Testing/main.cpp
@@ -8,5 +8,7 @@ int main()
     cin >> c1 >> c2 >> c3 >> c4 >> c5;
     cout << "ASCII message: " <<int(c1) << " " <<int(c2) << " " <<int(c3)
      << " " <<int(c4) << " " <<int(c5) << endl;
+    cout << char(int(c4)) << endl;
+    cout << int(c4) <<endl;
 system("pause.0");
 }
